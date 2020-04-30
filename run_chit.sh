917a58b22c833689d4a8ecd5e5fb46c1a2a582e6
QiYan <44687737+QiYanPitt@users.noreply.github.com>
2020-04-29 20:35:10 -0400

    Update run_chit.sh

--- run_chit.sh
+++ run_chit.sh
@@ -1,4 +1,3 @@
-# Use two different initial values
 for i in {1..22}; do
     CHIT_IN_FILE=./RNAseq/cht_input_file_atopy_ext.ranphe.txt.$i
     nohup python ./CHIT/CHIT.py \
@@ -8,14 +7,3 @@ for i in {1..22}; do
         $CHIT_IN_FILE ./RNAseq/output/chit_atopy_results.txt.$i &
     sleep 1
 done
-
-for i in {1..22}; do
-    CHIT_IN_FILE=./RNAseq/cht_input_file_atopy_ext.ranphe.txt.$i
-    nohup python ./CHIT/CHIT.py --initial 50 \
-        --bnb_disp ./RNAseq/cht_bnb_coef_atopy.txt \
-        --as_disp ./RNAseq/cht_as_coef_atopy.txt \
-        --cov_file ./pca/cov.matrix.txt \
-        $CHIT_IN_FILE ./RNAseq/output/chit_atopy_results.rerun.txt.$i &
-    sleep 1
-done
-
